5e8571f077fe6e5ba47b28889ae7a0bd1dcefb7c
evgenyfokin <fokin_e@inbox.ru>
2023-07-28 17:03:05 +0300

    fix

--- models/index.js
+++ models/index.js
@@ -11,53 +11,76 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {'use strict';
-    const {
-      Model
-    } = require('sequelize');
-    module.exports = (sequelize, DataTypes) => {
-      class User extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-          // define association here
+const dataBaseUrl = process.env.CLEARDB_DATABASE_URL
+
+if (databaseUrl) {
+    const connectionParams = new URL(databaseUrl); // Разбиваем URL на компоненты
+    sequelize = new Sequelize(connectionParams.pathname.substr(1), connectionParams.username, connectionParams.password, {
+        host: connectionParams.hostname,
+        dialect: 'mysql',
+        protocol: 'mysql',
+        port: connectionParams.port,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Важно добавить эту опцию, если сервер базы данных использует самоподписанный сертификат
+            }
         }
-      }
-      User.init({
-        name: DataTypes.STRING
-      }, {
-        sequelize,
-        modelName: 'User',
-      });
-      return User;
-    };
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+    });
+} else {
+    const env = process.env.NODE_ENV || 'development';
+    const config = require(__dirname + '/../config/config.json')[env];
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
+    })
+    .forEach(file => {
+        'use strict';
+        const {
+            Model
+        } = require('sequelize');
+        module.exports = (sequelize, DataTypes) => {
+            class User extends Model {
+                /**
+                 * Helper method for defining associations.
+                 * This method is not a part of Sequelize lifecycle.
+                 * The `models/index` file will call this method automatically.
+                 */
+                static associate(models) {
+                    // define association here
+                }
+            }
+
+            User.init({
+                name: DataTypes.STRING
+            }, {
+                sequelize,
+                modelName: 'User',
+            });
+            return User;
+        };
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
